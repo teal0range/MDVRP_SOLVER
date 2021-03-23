0444cb66b867a2c318b2f1b3c3a9325a269db6d2
Isolocoder <46982753+Isolocoder@users.noreply.github.com>
2021-03-23 09:56:37 +0800

    refactor framework

--- src/main/java/Operators/InnerShift10.java
+++ src/main/java/Operators/InnerShift10.java
@@ -5,13 +5,10 @@ import Common.Problem;
 import Common.Route;
 import Common.Solution;
 import Constraints.HardConstraint;
-import Constraints.InnerShift10.HardShift10ConsManager;
 
 import java.util.ArrayList;
 
 public class InnerShift10 extends InnerOperator{
-
-    HardShift10ConsManager consManager = new HardShift10ConsManager();
 
     public InnerShift10(Problem problem) {
         super(problem);
@@ -31,14 +28,6 @@ public class InnerShift10 extends InnerOperator{
     }
 
     public void executeValidOperation(Route route){
-        if (route.getLength()==1)return;
-        operateContext context = new operateContext.Builder(operateContext.operatorType.SHIFT10,route,new Node[2]).build();
-        for (int i = 0; i < route.getLength(); i++) {
-            for (int j = i + 1; j < route.getLength(); j++) {
-                context.setOperateNodes(0,route.getNode(i)).setOperateNodes(1,route.getNode(j));
-                if (consManager.fulfilled(context)== HardConstraint.ConsStatus.FULFILLED)singleOperate(context);
-            }
-        }
     }
 
 }
