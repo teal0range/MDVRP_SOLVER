b579b390a1930cac22f14eb53bfd03d43c40492d
Isolocoder <46982753+Isolocoder@users.noreply.github.com>
2021-03-24 10:30:14 +0800

    fix:parameter mismatch

--- src/main/java/Common/Node/Depot.java
+++ src/main/java/Common/Node/Depot.java
@@ -4,7 +4,7 @@ public class Depot extends Node{
     public int maxVehicleLoad;
     public int maxDuration;
 
-    public Depot(int id, double x, double y, int duration, int maxVehicleLoad, int maxDuration) {
+    public Depot(int id, double x, double y, int duration, int maxDuration, int maxVehicleLoad) {
         super(id, x, y, duration);
         this.maxVehicleLoad = maxVehicleLoad;
         this.maxDuration = maxDuration;
